3b30988e5773994146c0ea13305a40473237bc40
FangDashuai <60924962+FangDashuai@users.noreply.github.com>
2020-06-25 22:10:39 +0800

    Add files via upload

---
+++ Final-Homework/Final-Homework/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Final_Homework.Models;
+
+namespace Final_Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Statistics()
+        {
+            return View("Statistics");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
